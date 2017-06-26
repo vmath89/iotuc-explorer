79ba57c0d0f07ee3facfe07863e46dcf6acc86f5
Vishesh <vmath89@gmail.com>
2017-06-26 11:58:01 +0200

    dashboard initial phase done

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -20,21 +20,27 @@ export class DashboardComponent implements OnInit {
   ) { }
 
   public fillTiles(): void {
-      console.log(this.industryObj.length);
+      console.log(this.industryObj);
+
       this.tiles = [
-          {text: 'One', cols: 1, rows: 2, color: 'lightblue', header: 'Header1', footer: 'Footer1'},
-          {text: 'Two', cols: 1, rows: 2, color: 'lightblue', header: 'Header2', footer: 'Footer2'},
-          {text: 'Three', cols: 1, rows: 2, color: 'lightblue', header: 'Header3', footer: 'Footer3'},
-          {text: 'Four', cols: 1, rows: 2, color: 'lightblue', header: 'Header4', footer: 'Footer4'},
+          {cols: 1, rows: 2, color: 'lightblue', header: 'Header1', footer: 'Footer1'},
+          {cols: 1, rows: 2, color: 'lightblue', header: 'Header2', footer: 'Footer2'},
+          {cols: 1, rows: 2, color: 'lightblue', header: 'Header3', footer: 'Footer3'},
+          {cols: 1, rows: 2, color: 'lightblue', header: 'Header4', footer: 'Footer4'},
+          {cols: 1, rows: 2, color: 'lightblue', header: 'Header4', footer: 'Footer4'},
       ];
+
+      for(var i = 0; i < this.industryObj.length; i++){
+          this.tiles[i].text = this.industryObj[i].name;
+          this.tiles[i].header = this.industryObj[i].description;
+          this.tiles[i].footer = this.industryObj[i].description;
+      }
   }
 
 
   ngOnInit() {
       this.industryService.getIndustries().subscribe(res =>{
           this.industryObj = res.json()
-          this.industryObj = JSON.stringify(this.industryObj);
-          console.log(this.industryObj);
           this.fillTiles();
       });
 
